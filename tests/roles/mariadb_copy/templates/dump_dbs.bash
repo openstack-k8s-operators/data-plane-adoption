7ace2287c61b290da3ce7a63fbd94aeaf17f4469
Sean Mooney <work@seanmooney.info>
2023-08-10 07:23:20 -0400

    extend mariadb_copy to split out nova cell1 into a seperate db
    This change modifes the default contolplane CR to deploy two mariadb
    instances, the common openstack db and a seperate openstack-cell1
    db. The later will be used to adopt the cell1 db called "nova" into
    a seperate db instance. this logic is generic and has replaced the
    prior rename logic for neutron.
    
    This change extends the extends the mariadb_copy doc and test
    to split up the resotre of the db into nova and non_nova dbs.
    the non nova dbs are all adopted to the shared openstack db instance.
    the nova_api and nova_cell0 db are adopted to the openstack db
    and the nova db is adopted into the openstack-cell1 db.

---
+++ tests/roles/mariadb_copy/templates/dump_dbs.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+{{ shell_header }}
+{{ oc_header }}
+{{ mariadb_copy_shell_vars }}
+
+mkdir -p {{ mariadb_copy_tmp_dir }}
+cd {{ mariadb_copy_tmp_dir }}
+
+podman run -i --rm --userns=keep-id -u $UID -v $PWD:$PWD:z,rw -w $PWD $MARIADB_IMAGE bash <<EOF
+
+# Note we do not want to dump the information and performance schema tables so we filter them
+mysql -h ${SOURCE_MARIADB_IP} -u root "-p${SOURCE_DB_ROOT_PASSWORD}" -N -e 'show databases' | grep -E -v 'schema|mysql' | while read dbname; do
+    echo "Dumping \${dbname}"
+    mysqldump -h $SOURCE_MARIADB_IP -uroot "-p$SOURCE_DB_ROOT_PASSWORD" \
+        --single-transaction --complete-insert --skip-lock-tables --lock-tables=0 \
+        "\${dbname}" > "\${dbname}".sql
+done
+
+EOF
---
+++ tests/roles/mariadb_copy/templates/restore_dbs.bash
@@ -0,0 +1,47 @@
+#!/bin/bash
+{{ shell_header }}
+{{ oc_header }}
+{{ mariadb_copy_shell_vars }}
+cd {{ mariadb_copy_tmp_dir }}
+
+# db schemas to rename on import
+declare -A db_name_map
+db_name_map["nova"]="nova_cell1"
+db_name_map["ovs_neutron"]="neutron"
+
+# db servers to import into
+declare -A db_server_map
+db_server_map["default"]=${PODIFIED_MARIADB_IP}
+db_server_map["nova_cell1"]=${PODIFIED_CELL1_MARIADB_IP}
+
+# db server root password map
+declare -A db_server_password_map
+db_server_password_map["default"]=${PODIFIED_DB_ROOT_PASSWORD}
+db_server_password_map["nova_cell1"]=${PODIFIED_DB_ROOT_PASSWORD}
+
+all_db_files=$(ls *.sql)
+for db_file in ${all_db_files}; do
+    db_name=$(echo ${db_file} | awk -F'.' '{ print $1; }')
+    if [[ -v "db_name_map[${db_name}]" ]]; then
+        echo "renaming ${db_name} to ${db_name_map[${db_name}]}"
+        db_name=${db_name_map[${db_name}]}
+    fi
+    db_server=${db_server_map["default"]}
+    if [[ -v "db_server_map[${db_name}]" ]]; then
+        db_server=${db_server_map[${db_name}]}
+    fi
+    db_password=${db_server_password_map["default"]}
+    if [[ -v "db_server_password_map[${db_name}]" ]]; then
+        db_password=${db_server_password_map[${db_name}]}
+    fi
+    echo "creating ${db_name} in ${db_server}"
+    container_name=$(echo "mariadb-client-${db_name}-create" | sed 's/_/-/g')
+    oc run ${container_name} --image ${MARIADB_IMAGE} -i --rm --restart=Never -- \
+        mysql -h "${db_server}" -uroot "-p${db_password}" << EOF
+CREATE DATABASE IF NOT EXISTS ${db_name} DEFAULT CHARACTER SET ${CHARACTER_SET} DEFAULT COLLATE ${COLLATION};
+EOF
+    echo "importing ${db_name} into ${db_server}"
+    container_name=$(echo "mariadb-client-${db_name}-restore" | sed 's/_/-/g')
+    oc run ${container_name} --image ${MARIADB_IMAGE} -i --rm --restart=Never -- \
+        mysql -h "${db_server}" -uroot "-p${db_password}" "${db_name}" < "${db_file}"
+done
