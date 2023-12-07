164b9e46b22bf683cf8217bcc9eb3d289e6f671f
r <pkomarov@redhat.com>
2023-12-07 21:02:50 +0200

    add ssh to remote host capability

--- tests/roles/mariadb_copy/templates/pre_checks.bash
+++ tests/roles/mariadb_copy/templates/pre_checks.bash
@@ -2,7 +2,9 @@
 {{ shell_header }}
 {{ oc_header }}
 {{ mariadb_copy_shell_vars }}
-
+# TODO:
+{{  ssh_to_ospdo_openstackclient}}
+{{ ssh_to_ospdo_osp_controller  }}
 # Test connection to the original DB (show databases)
 podman run -i --rm --userns=keep-id -u $UID -v $PWD:$PWD:z,rw -w $PWD $MARIADB_IMAGE \
     mysql -h "$SOURCE_MARIADB_IP" -uroot "-p$SOURCE_DB_ROOT_PASSWORD" -e 'SHOW databases;'
@@ -14,3 +16,4 @@ oc run mariadb-client --image $MARIADB_IMAGE -i --rm --restart=Never -- \
     mysql -h "$PODIFIED_MARIADB_IP" -uroot "-p$PODIFIED_DB_ROOT_PASSWORD" -e 'SHOW databases;'
 oc run mariadb-client --image $MARIADB_IMAGE -i --rm --restart=Never -- \
     mysql -h "$PODIFIED_CELL1_MARIADB_IP" -uroot "-p$PODIFIED_DB_ROOT_PASSWORD" -e 'SHOW databases;'
+'"
