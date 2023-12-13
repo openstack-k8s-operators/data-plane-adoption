761c945d8331a5884e54cb060f0eb281ea632eed
r <pkomarov@redhat.com>
2023-12-13 16:57:11 +0200

    ext shell for galara exec

--- tests/roles/mariadb_copy/templates/dump_dbs.bash
+++ tests/roles/mariadb_copy/templates/dump_dbs.bash
@@ -6,12 +6,10 @@
 {{ ssh_to_ospdo_osp_controller }}
 # mkdir -p {{ mariadb_copy_tmp_dir }}
 # cd {{ mariadb_copy_tmp_dir }}
-sudo -i
-maria_con=$(podman ps -f name=galera --format="{{.Names}}")
 
 # podman run -i --rm --userns=keep-id -u $UID -v $PWD:$PWD:z,rw -w $PWD $MARIADB_IMAGE bash <<EOF
 
-podman exec -t $maria_con bash -c "
+sudo podman exec -t $GALERA_CON_NAME bash -c "
 # Note we do not want to dump the information and performance schema tables so we filter them
 mysql -h ${SOURCE_MARIADB_IP} -u root "-p${SOURCE_DB_ROOT_PASSWORD}" -N -e 'show databases' | grep -E -v 'schema|mysql' | while read dbname; do
     echo "Dumping \${dbname}"
