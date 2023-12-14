d577934ad87be821c1578f661dbec7bee5dad3d0
r <pkomarov@redhat.com>
2023-12-15 00:41:54 +0200

    finalized mysql dump

--- tests/roles/mariadb_copy/templates/dump_dbs.bash
+++ tests/roles/mariadb_copy/templates/dump_dbs.bash
@@ -1,21 +1,17 @@
-#!/bin/bash
-{{ shell_header }}
-{{ oc_header }}
-{{ mariadb_copy_shell_vars }}
-{{ ssh_to_ospdo_openstackclient }}
-{{ ssh_to_ospdo_osp_controller }}
+# #!/bin/bash
+# {{ shell_header }}
+# {{ oc_header }}
+# {{ mariadb_copy_shell_vars }}
+# {{ ssh_to_ospdo_openstackclient }}
+# {{ ssh_to_ospdo_osp_controller }}
+# "
 # mkdir -p {{ mariadb_copy_tmp_dir }}
 # cd {{ mariadb_copy_tmp_dir }}
-
-# podman run -i --rm --userns=keep-id -u $UID -v $PWD:$PWD:z,rw -w $PWD $MARIADB_IMAGE bash <<EOF
-
-sudo podman exec -t $GALERA_CON_NAME bash -c "
-# Note we do not want to dump the information and performance schema tables so we filter them
-mysql -h ${SOURCE_MARIADB_IP} -u root "-p${SOURCE_DB_ROOT_PASSWORD}" -N -e 'show databases' | grep -E -v 'schema|mysql' | while read dbname; do
-    echo "Dumping \${dbname}"
-    mysqldump -h $SOURCE_MARIADB_IP -uroot "-p$SOURCE_DB_ROOT_PASSWORD" \
-        --single-transaction --complete-insert --skip-lock-tables --lock-tables=0 \
-        "\${dbname}" > "/var/log/mariadb/\${dbname}".sql
-done
-" # podman exec
-'" # ssh_openstackclient & controller
+# mysql -h ${SOURCE_MARIADB_IP} -u root "-p${SOURCE_DB_ROOT_PASSWORD}" -N -e 'show databases' | grep -E -v 'schema|mysql' | while read dbname; do
+#     echo "Dumping \${dbname}"
+#     mysqldump -h $SOURCE_MARIADB_IP -uroot "-p$SOURCE_DB_ROOT_PASSWORD" \
+#         --single-transaction --complete-insert --skip-lock-tables --lock-tables=0 \
+#         "\${dbname}" > "{{ mariadb_copy_tmp_dir }}/\${dbname}".sql
+# done
+# " # ctrl0 cli
+# '" # ssh_openstackclient & controller
