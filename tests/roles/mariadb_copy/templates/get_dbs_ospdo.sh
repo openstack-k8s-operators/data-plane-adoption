f4af461079dce66232bfa4d5c71513370b7f13a4
r <pkomarov@redhat.com>
2024-01-15 19:55:51 +0200

    switch to galera con mysql use

--- tests/roles/mariadb_copy/templates/get_dbs_ospdo.sh
+++ tests/roles/mariadb_copy/templates/get_dbs_ospdo.sh
@@ -4,4 +4,4 @@ galera_con=$(sudo podman ps --format="{{.Names}}"|grep galera)
 mariadb_templates='/home/cloud-admin/data-plane-adoption_pkomarov/tests/roles/mariadb_copy/templates/'
 
 sudo podman cp ${mariadb_templates}/dump_dbs_ospdo.sh $galera_con:/
-sudo podman exec -it $galera_con bash -c "/mysql_db_dump.sh $1 $2"
+sudo podman exec -it $galera_con bash -c "/dump_dbs_ospdo.sh $1 $2"
