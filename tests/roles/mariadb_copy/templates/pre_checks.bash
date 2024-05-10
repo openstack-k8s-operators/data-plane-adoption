c3ba4c36f2474bb142591030a4591e8b32607493
Bohdan Dobrelia <bdobreli@redhat.com>
2024-05-10 13:21:00 +0200

    Multi-cell adoption
    Keep renaming 'default' cell consistent for single and multi cells:
    
    * Default becomes cellX (or it can be imported as is, for a multi-cell
      case only)
    * cell1 becomes mapped to openstack-cell1 osdp node set
    * cell2 becomes mapped to openstack-cell2 osdp node set, etc.
    * cellX (X=3 here) becomes mapped to openstack-cell3. Alternatively,
      default cell retains its name for the openstack-default osdpns
      mapping
    
    Evaluate podified MariaDB passwords for cells from osp-secret
    to align the tests with documented commands. Remove no longer
    needed podified DB password variable.
    
    Make ansible and shell variables compute cells aware.
    Split edpm nodes into compute cells by 1:1 mapping it as
    dataplane nodesets.
    
    Rework vars and secrets YAML values for the source and edpm
    nodes to not confuse its different naming schemes for cells
    in OSP/TripleO and RHOSO.
    
    Use edpm_nodes var to describe compuptes for each cell,
    instead of static host and ip vars that only used to work for
    a single-cell standalone, or multi-node single cell cases.
    Also explain EDPM net config requirements in vars.sample, when
    it is used outside of ci-framework (local deployments).
    
    Remove edpm_computes vars no longer used after moving stopping
    control-plane tripleo services into edpm-ansible
    
    Remove cached fact for pulled OSP configuration as it can no longer
    be generated in a multi-cell setup, where related shell variables
    become bash arrays.
    
    Simplify ENV headers management by collecting in a single place.
    
    Provide a variable to define the source cloud Ironic topology,
    for any cells with Ironic services.
    
    Align nova/libvirt and related services ordering in the
    lists of services defined in multiple places, with those
    specified in VA.
    
    Add a missing step in the fast forward uprgade guide
    to complete the adoption of the remaining dataplane services.
    
    Align the names in the tests to follow the documented steps
    to make the corresponding code easy discoverable.
    
    Adjust storage/storageRequests values to make it better fitting
    a multi-cell test scenarios. Also provide values in docs and
    add a comment to adjust them as needed.
    
    Stop ovn services only if active, or not missing (like on
    the cell controllers)
    
    Signed-off-by: Bohdan Dobrelia <bdobreli@redhat.com>

--- tests/roles/mariadb_copy/templates/pre_checks.bash
+++ tests/roles/mariadb_copy/templates/pre_checks.bash
@@ -1,11 +1,8 @@
 #!/bin/bash
-{{ shell_header }}
-{{ oc_header }}
-{{ mariadb_copy_shell_vars_src }}
 {{ mariadb_copy_shell_vars_dst }}
 
-# Test connection to podified DBs (show databases)
-oc run mariadb-client --image $MARIADB_IMAGE -i --rm --restart=Never -- \
-    mysql -rsh "$PODIFIED_MARIADB_IP" -uroot -p"$PODIFIED_DB_ROOT_PASSWORD" -e 'SHOW databases;'
-oc run mariadb-client --image $MARIADB_IMAGE -i --rm --restart=Never -- \
-    mysql -rsh "$PODIFIED_CELL1_MARIADB_IP" -uroot -p"$PODIFIED_DB_ROOT_PASSWORD" -e 'SHOW databases;'
+# Test the connection to the control plane "upcall" and cells' databases
+for CELL in $(echo "super $RENAMED_CELLS"); do
+  oc run mariadb-client --image $MARIADB_IMAGE -i --rm --restart=Never -- \
+    mysql -rsh "${PODIFIED_MARIADB_IP[$CELL]}" -uroot -p"${PODIFIED_DB_ROOT_PASSWORD[$CELL]}" -e 'SHOW databases;'
+done
