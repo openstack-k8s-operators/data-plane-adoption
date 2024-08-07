a08ae8fe806ccf3037b9bb9ab9dd648c83308ba1
Yadnesh Kulkarni <ykulkarn@redhat.com>
2024-08-07 16:54:37 +0530

    Do not backup Aodh database from previous release
    Since migrating Aodh alarms from previous release is not
    suported there is no good reason to restore the backup.

--- tests/roles/mariadb_copy/templates/dump_dbs.bash
+++ tests/roles/mariadb_copy/templates/dump_dbs.bash
@@ -5,9 +5,10 @@
 
 # Note Filter the information and performance schema tables
 # Gnocchi is no longer used as a metric store, skip dumping gnocchi database as well
+# Migrating Aodh alarms from previous release is not supported, hence skip aodh database
 oc rsh mariadb-copy-data << EOF
   mysql -h"${SOURCE_MARIADB_IP}" -uroot -p"${SOURCE_DB_ROOT_PASSWORD}" \
-  -N -e "show databases" | grep -E -v "schema|mysql|gnocchi" | \
+  -N -e "show databases" | grep -E -v "schema|mysql|gnocchi|aodh" | \
   while read dbname; do
     echo "Dumping \${dbname}";
     mysqldump -h"${SOURCE_MARIADB_IP}" -uroot -p"${SOURCE_DB_ROOT_PASSWORD}" \
