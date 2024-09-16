2a472f6c13b92cf8f5d63b2cb94b8b3baa9e9339
Pini Komarov <pkomarov@redhat.com>
2024-09-16 23:05:54 +0300

    backend_services ospdo update

--- tests/roles/mariadb_copy/templates/dump_dbs.bash
+++ tests/roles/mariadb_copy/templates/dump_dbs.bash
@@ -1,13 +1,11 @@
 #!/bin/bash
 {{ shell_header }}
-if [[ "$ospdo_src" != "true" ]]; then
-    {{ oc_header }}
-fi
+{{ oc_header }}
 {{ mariadb_copy_shell_vars_src }}
 
 # Note Filter the information and performance schema tables
 # Gnocchi is no longer used as a metric store, skip dumping gnocchi database as well
-oc rsh {{ mariadb_clnt_pod_name }} << EOF
+oc rsh mariadb-copy-data << EOF
   mysql -h"${SOURCE_MARIADB_IP}" -uroot -p"${SOURCE_DB_ROOT_PASSWORD}" \
   -N -e "show databases" | grep -E -v "schema|mysql|gnocchi|aodh" | \
   while read dbname; do
