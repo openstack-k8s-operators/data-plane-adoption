10362ed7d3679b7f17e36203aca100fb51b09f64
Pini Komarov <pkomarov@redhat.com>
2024-10-31 14:00:51 +0200

    update vars

--- tests/roles/mariadb_copy/templates/dump_dbs.bash
+++ tests/roles/mariadb_copy/templates/dump_dbs.bash
@@ -6,7 +6,7 @@
 # Note Filter the information and performance schema tables
 # Gnocchi is no longer used as a metric store, skip dumping gnocchi database as well
 # Migrating Aodh alarms from previous release is not supported, hence skip aodh database
-oc rsh  -n {{ org_namespace| default('openstack') }} mariadb-copy-data << EOF
+oc rsh  -n "{{ org_namespace }}" mariadb-copy-data << EOF
   mysql -h"${SOURCE_MARIADB_IP}" -uroot -p"${SOURCE_DB_ROOT_PASSWORD}" \
   -N -e "show databases" | grep -E -v "schema|mysql|gnocchi|aodh" | \
   while read dbname; do
