abbb47b81cbf4db99be7877d042161dc37864a08
Pini Komarov <pkomarov@redhat.com>
2024-11-07 22:36:47 +0200

    fix typo

--- tests/roles/mariadb_copy/templates/pre_checks.bash
+++ tests/roles/mariadb_copy/templates/pre_checks.bash
@@ -6,6 +6,6 @@
 
 # Test connection to podified DBs (show databases)
 oc run mariadb-client -n {{ rhoso_namespace }} ${MARIADB_CLIENT_ANNOTATIONS} --image $MARIADB_IMAGE -i --rm --restart=Never -- \
-    mysql -rsh "$PODIFIED_MARIADB_IP" -n {{ rhoso_namespace }} -uroot -p"$PODIFIED_DB_ROOT_PASSWORD" -e 'SHOW databases;'
+    mysql -rsh "$PODIFIED_MARIADB_IP" -uroot -p"$PODIFIED_DB_ROOT_PASSWORD" -e 'SHOW databases;'
 oc run mariadb-client -n {{ rhoso_namespace }} ${MARIADB_CLIENT_ANNOTATIONS} --image $MARIADB_IMAGE -i --rm --restart=Never -- \
     mysql -rsh "$PODIFIED_CELL1_MARIADB_IP" -uroot -p"$PODIFIED_DB_ROOT_PASSWORD" -e 'SHOW databases;'
