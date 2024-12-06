7f5f9912dd29edcd89be5d01e63a0916536b6c91
Bohdan Dobrelia <bdobreli@redhat.com>
2024-12-06 17:17:47 +0100

    Refactor comments in commands into asciidocs native
    Illustrate how commands in scripts could have comments
    that become (almost as is) native ascii docs foot-notes.
    
    When copying code into docs, the minimal adjustments will
    be needed, like adding '$' prefix (or '>' for multiline commands).
    
    Signed-off-by: Bohdan Dobrelia <bdobreli@redhat.com>

--- tests/roles/mariadb_copy/templates/restore_dbs.bash
+++ tests/roles/mariadb_copy/templates/restore_dbs.bash
@@ -8,44 +8,32 @@ for CELL in $(echo $CELLS); do
   RCELL=$CELL
   [ "$CELL" = "default" ] && RCELL=$DEFAULT_CELL_NAME
   oc rsh -n $NAMESPACE mariadb-copy-data << EOF
-    # db schemas to rename on import
-    declare -A db_name_map
+    declare -A db_name_map # <1>
     db_name_map['nova']="nova_$RCELL"
     db_name_map['ovs_neutron']='neutron'
     db_name_map['ironic-inspector']='ironic_inspector'
-
-    # cells' db schemas to import for cells
-    declare -A db_cell_map
+    declare -A db_cell_map # <2>
     db_cell_map['nova']="nova_$DEFAULT_CELL_NAME"
-    db_cell_map["nova_$RCELL"]="nova_$RCELL"
-    # Omit importing cells' cell0 DBs as we cannot consolidate them
-    # db_cell_map["nova_cell0"]="nova_$RCELL"
-
-    # db servers to import into
-    declare -A db_server_map
+    db_cell_map["nova_$RCELL"]="nova_$RCELL" # <3>
+    declare -A db_server_map # <4>
     db_server_map['default']=${PODIFIED_MARIADB_IP['super']}
     db_server_map["nova"]=${PODIFIED_MARIADB_IP[$DEFAULT_CELL_NAME]}
     db_server_map["nova_$RCELL"]=${PODIFIED_MARIADB_IP[$RCELL]}
-
-    # db server root password map
-    declare -A db_server_password_map
+    declare -A db_server_password_map # <5>
     db_server_password_map['default']=${PODIFIED_DB_ROOT_PASSWORD['super']}
     db_server_password_map["nova"]=${PODIFIED_DB_ROOT_PASSWORD[$DEFAULT_CELL_NAME]}
     db_server_password_map["nova_$RCELL"]=${PODIFIED_DB_ROOT_PASSWORD[$RCELL]}
-
     cd /backup
     for db_file in \$(ls ${CELL}.*.sql); do
       db_name=\$(echo \${db_file} | awk -F'.' '{ print \$2; }')
-      # Only import cells' DBs and omit everything else
       [[ "$CELL" != "default" && ! -v "db_cell_map[\${db_name}]" ]] && continue
-      # Route databases for importing, when extracting cell's / non-cell's DBs from 'default' cell
       if [[ "$CELL" == "default" && -v "db_cell_map[\${db_name}]" ]] ; then
         target=$DEFAULT_CELL_NAME
       elif [[ "$CELL" == "default" && ! -v "db_cell_map[\${db_name}]" ]] ; then
-        target=super # 'upcall'
+        target=super
       else
         target=$RCELL
-      fi
+      fi # <6>
       renamed_db_file="\${target}_new.\${db_name}.sql"
       mv -f \${db_file} \${renamed_db_file}
       if [[ -v "db_name_map[\${db_name}]" ]]; then
@@ -67,7 +55,6 @@ for CELL in $(echo $CELLS); do
       echo "importing $CELL cell \${db_name} into \$target \${db_server} from \${renamed_db_file}"
       mysql -h "\${db_server}" -uroot "-p\${db_password}" "\${db_name}" < "\${renamed_db_file}"
     done
-
     if [ "$CELL" = "default" ] ; then
       mysql -h "\${db_server_map['default']}" -uroot -p"\${db_server_password_map['default']}" -e \
         "update nova_api.cell_mappings set name='$DEFAULT_CELL_NAME' where name='default';"
@@ -76,3 +63,10 @@ for CELL in $(echo $CELLS); do
       "delete from nova_${RCELL}.services where host not like '%nova_${RCELL}-%' and services.binary != 'nova-compute';"
 EOF
 done
+
+# <1> Defines which common databases to rename, when importing it
+# <2> Defines which cells' databases to import, and how to rename it, if needed.
+# <3> Omits importing cells' special `cell0` databases as we cannot consolidate its contents during adoption.
+# <4> Defines which databases to import into which servers (usually dedicated for cells).
+# <5> Defines root passwords map for database servers (we can only use the same password for now).
+# <6> Asigns which databases to import into which hosts, when extracting databases from `default` cell.
