cfb487ebf72e1cc13a91cf33f73c5716ebcc6c5d
Bohdan Dobrelia <bdobreli@redhat.com>
2024-12-06 17:17:47 +0100

    Refactor comments in commands into asciidocs native
    Illustrate how commands in scripts could have comments
    that become (almost as is) native ascii docs foot-notes.
    
    When copying code into docs, the minimal adjustments will
    be needed, like adding '$' prefix (or '>' for multiline commands).
    
    Signed-off-by: Bohdan Dobrelia <bdobreli@redhat.com>

--- tests/roles/mariadb_copy/templates/post_checks.bash
+++ tests/roles/mariadb_copy/templates/post_checks.bash
@@ -5,17 +5,12 @@
 set +u
 . ~/.source_cloud_exported_variables_default
 set -u
-
 dbs=$(oc exec openstack-galera-0 -n $NAMESPACE -c galera -- mysql -rs -uroot -p"${PODIFIED_DB_ROOT_PASSWORD['super']}" -e 'SHOW databases;')
 echo $dbs | grep -Eq '\bkeystone\b' && echo "OK" || echo "CHECK FAILED"
-
-# ensure neutron db is renamed from ovs_neutron
 echo $dbs | grep -Eq '\bneutron\b'
-echo "${PULL_OPENSTACK_CONFIGURATION_DATABASES[@]}" | grep -Eq '\bovs_neutron\b' && echo "OK" || echo "CHECK FAILED"
-
-# ensure default cell is renamed to $DEFAULT_CELL_NAME, and the cell UUIDs retained intact
+echo "${PULL_OPENSTACK_CONFIGURATION_DATABASES[@]}" | grep -Eq '\bovs_neutron\b' && echo "OK" || echo "CHECK FAILED" # <1>
 novadb_mapped_cells=$(oc exec openstack-galera-0 -n $NAMESPACE -c galera -- mysql -rs -uroot -p"${PODIFIED_DB_ROOT_PASSWORD['super']}" \
-  nova_api -e 'select uuid,name,transport_url,database_connection,disabled from cell_mappings;')
+  nova_api -e 'select uuid,name,transport_url,database_connection,disabled from cell_mappings;') # <2>
 uuidf='\S{8,}-\S{4,}-\S{4,}-\S{4,}-\S{12,}'
 left_behind=$(comm -23 \
   <(echo $PULL_OPENSTACK_CONFIGURATION_NOVADB_MAPPED_CELLS | grep -oE " $uuidf \S+") \
@@ -27,9 +22,7 @@ test $(grep -Ec ' \S+$' <<<$left_behind) -eq 1 && echo "OK" || echo "CHECK FAILE
 default=$(grep -E ' default$' <<<$left_behind)
 test $(grep -Ec ' \S+$' <<<$changed) -eq 1 && echo "OK" || echo "CHECK FAILED"
 grep -qE " $(awk '{print $1}' <<<$default) ${DEFAULT_CELL_NAME}$" <<<$changed && echo "OK" || echo "CHECK FAILED"
-
-# ensure the registered Compute service names have not changed
-for CELL in $(echo $RENAMED_CELLS); do
+for CELL in $(echo $RENAMED_CELLS); do # <3>
   unset PULL_OPENSTACK_CONFIGURATION_DATABASES
   unset PULL_OPENSTACK_CONFIGURATION_MYSQLCHECK_NOK
   unset PULL_OPENSTACK_CONFIGURATION_NOVA_COMPUTE_HOSTNAMES
@@ -41,12 +34,15 @@ for CELL in $(echo $RENAMED_CELLS); do
   set +u
   . ~/.source_cloud_exported_variables_$RCELL
   set -u
-  # ensure nova cells' db are extracted to separate db servers and renamed from nova to nova_cell<X>
-  c1dbs=$(oc exec openstack-$CELL-galera-0 -n $NAMESPACE -c galera -- mysql -rs -uroot -p${PODIFIED_DB_ROOT_PASSWORD[$CELL]} -e 'SHOW databases;')
+  c1dbs=$(oc exec openstack-$CELL-galera-0 -n $NAMESPACE -c galera -- mysql -rs -uroot -p${PODIFIED_DB_ROOT_PASSWORD[$CELL]} -e 'SHOW databases;') # <4>
   echo $c1dbs | grep -Eq "\bnova_${CELL}\b" && echo "OK" || echo "CHECK FAILED"
-
-  # ensure the registered Compute service name has not changed
   novadb_svc_records=$(oc exec openstack-$CELL-galera-0 -n $NAMESPACE -c galera -- mysql -rs -uroot -p${PODIFIED_DB_ROOT_PASSWORD[$CELL]} \
     nova_$CELL -e "select host from services where services.binary='nova-compute' order by host asc;")
-  diff -Z <(echo "x$novadb_svc_records") <(echo "x${PULL_OPENSTACK_CONFIGURATION_NOVA_COMPUTE_HOSTNAMES[@]}") && echo "OK" || echo "CHECK FAILED"
+  diff -Z <(echo "x$novadb_svc_records") <(echo "x${PULL_OPENSTACK_CONFIGURATION_NOVA_COMPUTE_HOSTNAMES[@]}") && echo "OK" || echo "CHECK FAILED" # <5>
 done
+
+# <1> Ensures Neutron database is renamed from `ovs_neutron`
+# <2> Ensures `default`` cell is renamed to `$DEFAULT_CELL_NAME``, and the cell UUIDs retained intact
+# <3> Ensures the registered Compute services names have not changed
+# <4> Ensures Nova cells' DBs are extracted to separate database servers, and renamed from nova to nova_cell<X>
+# <5> Ensures the registered Compute service name has not changed
